215358f4ab21cc26b67120920f327043cb448e0b
Ernest G Jenkins <156819114+Ernest1230@users.noreply.github.com>
2024-02-08 10:31:55 -0500

    Add files via upload

---
+++ Feb-08.R
@@ -0,0 +1,10 @@
+#Author: Ernest Jenkins, Date: 02/08/2024, Purpose: Calculate the Correlation Test
+
+#Load the library ggpubr
+library(ggpubr)
+
+#Load some dummt dataset
+my_data <-mtcars
+
+#My laptop was unable to install a portion of the ggpubr library
+This lead to me not being able to create a plot
