7cc63a763a8702a7b621bf0c6a7fcaab9670d2fa
Ernest G Jenkins <156819114+Ernest1230@users.noreply.github.com>
2024-03-05 11:29:36 -0500

    Add files via upload

---
+++ Feb-27.R
@@ -0,0 +1,14 @@
+#Author: Ernest Jenkins, #Date: 02/27/2024, #Purpose: Use ANOVA on sample dataset
+
+#Load a library called dplyr
+library(dplyr)
+
+#Load a sample dataset
+PATH <- "https://raw.githubusercontent.com/guru99-edu/R-Programming/master/poisons.csv"
+
+df <-read.csv(PATH)
+
+#Apply ANOVA test
+anova_one_way <- aov(time~poison, data = df)
+summary(anova_one_way)
+
